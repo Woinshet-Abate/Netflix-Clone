5ef7e05212805e677bcd89e98d7c05aa8acc46a7
woin21 <woinshetabate2121@gmail.com>
2025-05-19 21:00:02 +0300

    update

--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -3,25 +3,32 @@ import axios from '../../Utils/axios/axios';
 import styles from './Row.module.css';
 import MovieCard from '../MovieCard/MovieCard';
 
-export default function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(fetchUrl);
-      setMovies(request.data.results);
+      const response = await axios.get(fetchUrl);
+      setMovies(response.data.results);
     }
     fetchData();
   }, [fetchUrl]);
 
   return (
     <div className={styles.row}>
-      <h2>{title}</h2>
+      <h2 className={styles.rowTitle}>{title}</h2>
       <div className={styles.rowPosters}>
         {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
+          <img
+            key={movie.id}
+            className={styles.rowPoster}
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.name || movie.title}
+          />
         ))}
       </div>
     </div>
   );
 }
+
+export default Row;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,15 +3,21 @@ import NavBar from '../../Components/NavBar/NavBar';
 import Banner from '../../Components/Banner/Banner';
 import Row from '../../Components/Row/Row';
 import requests from '../../Utils/request/request';
+import Footer from '../../Components/Footer/Footer';
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Banner />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+         <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Footer/>
     </>
   );
 }
