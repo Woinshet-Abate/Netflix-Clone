7876d165d4557effbc43aab718bd88512d39947b
woin21 <woinshetabate2121@gmail.com>
2025-05-19 23:59:11 +0300

    last update

--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -1,32 +1,59 @@
 import React, { useEffect, useState } from 'react';
+import movieTrailer from 'movie-trailer';
+import YouTube from "react-youtube"; 
 import axios from '../../Utils/axios/axios';
 import styles from './Row.module.css';
 import MovieCard from '../MovieCard/MovieCard';
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(fetchUrl);
-      setMovies(response.data.results);
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
     }
     fetchData();
   }, [fetchUrl]);
 
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl(""); // close trailer if already opened
+    } else {
+      movieTrailer(movie?.title || movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className={styles.row}>
-      <h2 className={styles.rowTitle}>{title}</h2>
       <div className={styles.rowPosters}>
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            className={styles.rowPoster}
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.name || movie.title}
-          />
-        ))}
-      </div>
+  {movies.map((movie) => (
+    <div key={movie.id} className={styles.movieItem}>
+      <img
+        onClick={() => handleClick(movie)}
+        className={styles.rowPoster}
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.name || movie.title}
+      />
+      <p className={styles.movieTitle}>{movie.title || movie.name}</p>
+    </div>
+  ))}
+</div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
